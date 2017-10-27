c7c2a5236a55cb68af9a0fc219f2de79e0d7a6ed
David Ly <dly5891@gmail.com>
2017-10-26 21:47:19 -0500

    Added some more components

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import {List, ListItem} from 'material-ui/List';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import {pinkA200, transparent} from 'material-ui/styles/colors';
 import AppBar from 'material-ui/AppBar';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
-// import logo from './logo.svg';
-// import './App.css';
-const style = {
-  display: 'inline-block',
-  margin: '0 32px 16px 0',
-};
+var style = {'float':'right'};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false
+    };
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => this.setState({ open: !this.state.open });
 
   render() {
+
+    //Hard coded values to showcase multiple animals
+    var list = [];
+    for (var i = 0; i < 20; i++) {
+      list.push(<MenuItem>Cat {i}</MenuItem>);
+    }
+
     return (
       <div className="App">
         <MuiThemeProvider>
           <AppBar
             title="Roll Call"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onLeftIconButtonTouchTap={this.handleToggle}
-          />
-          
-          <Drawer 
+            onLeftIconButtonTouchTap={this.handleToggle}>
+          </AppBar>
+
+          <Drawer
             docked={false}
             width={200}
             open={this.state.open}
-            onRequestChange={(open) => this.setState({open})}>
-              <MenuItem>Menu Item</MenuItem>
-              <MenuItem>Menu Item 2</MenuItem>
+            onRequestChange={(open) => this.setState({ open })}>
+            {list}
           </Drawer>
+
+          <Card>
+            <CardHeader
+              title="Cookie"
+            />
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            >
+            <img src="images/nature-600-337.jpg" alt="Test" />
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <RaisedButton label="Action1" style={style}/>
+              <RaisedButton label="Action2" style={style}/>
+            </CardActions>
+          </Card>
+
         </MuiThemeProvider>
       </div>
     );
