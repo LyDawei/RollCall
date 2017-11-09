80b4538d91532d37e288f485d18a0eeac69c8440
David Ly <dly5891@gmail.com>
2017-11-08 21:31:33 -0600

    Style changes and Check Out button functionality

--- src/components/CheckOutDialog.jsx
+++ src/components/CheckOutDialog.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import './CheckOutDialog.css';
 
-
-export default class CheckOutButton extends Component {
+export default class CheckOutDialog extends Component {
   render() {
     return (
-      <div>
+      <div class="checkout-button">
         <RaisedButton primary={true} label='Check Out' />
       </div>
     );
--- src/components/KennelCard.jsx
+++ src/components/KennelCard.jsx
@@ -2,28 +2,19 @@ import React, { Component } from 'react';
 import { Card, CardText } from 'material-ui/Card';
 import AnimalInfo from './AnimalInfo';
 import AnimalStory from './AnimalStory';
-import CheckOutButton from './CheckOutButton';
+import CheckOutDialog from './CheckOutDialog';
 import Polaroid from './Polaroid';
-
-var cardStyle = {
-  width: '100%',
-  float: 'left',
-  backgroundColor: '#f6f7f9'
-};
+import './KennelCard.css';
 
 class KennelCard extends Component {
   render() {
     return (
-      <div>
-        <Card>
-          <div style={{ display: 'table' }}>
-            <Polaroid />
-            <AnimalStory />
-            <AnimalInfo />
-          </div>
-          <CheckOutButton />
-        </Card>
-      </div>
+      <Card class="kennel-card">
+        <Polaroid />
+        <AnimalStory />
+        <AnimalInfo />
+        <CheckOutDialog />
+      </Card>
     );
   }
 }
